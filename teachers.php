51353a912c4df39fa93346e9a682487d25b4117c
schelpstech <sunkanmi@schelps.com.ng>
2023-04-21 16:15:44 +0100

    updated teachers profile

--- teachers.php
+++ teachers.php
@@ -19,7 +19,9 @@ include 'header.php';
           <div class="probootstrap-text probootstrap-animate">
             <h3>Mrs Irene Okwuegbuna Lofindipe </h3>
             <p>The Director</p>
-            <p>Mrs Lofindipe is an educator with over 20years experience, 10 at Corona School Apapa, and Gbagada. She a Bachelor of Arts in History(UI), postgraduate in Education Management from Cambridge, and has attended numerous workshops both locally and internationally </p>
+            <p>Mrs Lofindipe is an educator with over 20years experience, 10 at Corona School Apapa, and Gbagada. She a
+              Bachelor of Arts in History(UI), postgraduate in Education Management from Cambridge, and has attended
+              numerous workshops both locally and internationally </p>
             <p><a href="#" class="btn btn-primary">Learn More</a></p>
           </div>
           <div class="probootstrap-image probootstrap-animate" style="background-image: url(img/newpix/fpp.jpg)">
@@ -40,12 +42,12 @@ include 'header.php';
 
     <div class="row">
 
-      <div class="col-md-4 col-sm-6">
+    <div class="col-md-4 col-sm-6">
         <div class="probootstrap-teacher text-center probootstrap-animate">
 
           <div class="text">
             <h3>Mrs Atanda Brenda Abehi</h3>
-            <p>Class Teacher</p>
+            <p>Head Teacher / Year 4</p>
             <ul class="probootstrap-footer-social">
               <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
               <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
@@ -55,12 +57,13 @@ include 'header.php';
           </div>
         </div>
       </div>
+      
       <div class="col-md-4 col-sm-6">
         <div class="probootstrap-teacher text-center probootstrap-animate">
 
           <div class="text">
-            <h3>Miss Okorie Gloria</h3>
-            <p>Class Teacher</p>
+            <h3>Mr Chike Njoku</h3>
+            <p>School Administrator</p>
             <ul class="probootstrap-footer-social">
               <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
               <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
@@ -70,12 +73,15 @@ include 'header.php';
           </div>
         </div>
       </div>
+
+      
+
       <div class="col-md-4 col-sm-6">
         <div class="probootstrap-teacher text-center probootstrap-animate">
 
           <div class="text">
-            <h3>Mrs Sodipo Wumi</h3>
-            <p>Class Teacher</p>
+            <h3>Mrs Bimpe Jaiyeola</h3>
+            <p>Educational Consultant</p>
             <ul class="probootstrap-footer-social">
               <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
               <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
@@ -89,6 +95,132 @@ include 'header.php';
 
     </div>
 
+    <div class="row">
+
+      <div class="clearfix visible-sm-block visible-xs-block"></div>
+      <div class="col-md-3 col-sm-6">
+        <div class="probootstrap-teacher text-center probootstrap-animate">
+
+          <div class="text">
+            <h3>Mrs Wumi Sodipo</h3>
+            <p>Class Teacher - Year 2</p>
+            <ul class="probootstrap-footer-social">
+              <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
+              <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li class="instagram"><a href="#"><i class="icon-instagram2"></i></a></li>
+              <li class="google-plus"><a href="#"><i class="icon-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="probootstrap-teacher text-center probootstrap-animate">
+
+          <div class="text">
+            <h3>Miss Gift Okorie</h3>
+            <p>Class Teacher - Year 3</p>
+            <ul class="probootstrap-footer-social">
+              <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
+              <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li class="instagram"><a href="#"><i class="icon-instagram2"></i></a></li>
+              <li class="google-plus"><a href="#"><i class="icon-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="probootstrap-teacher text-center probootstrap-animate">
+
+          <div class="text">
+            <h3>Mrs Bamidele</h3>
+            <p>Class Teacher - Year 5</p>
+            <ul class="probootstrap-footer-social">
+              <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
+              <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li class="instagram"><a href="#"><i class="icon-instagram2"></i></a></li>
+              <li class="google-plus"><a href="#"><i class="icon-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="probootstrap-teacher text-center probootstrap-animate">
+
+          <div class="text">
+            <h3>Mrs Happiness Awotayo</h3>
+            <p>Class Teacher - Kindergarten 2</p>
+            <ul class="probootstrap-footer-social">
+              <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
+              <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li class="instagram"><a href="#"><i class="icon-instagram2"></i></a></li>
+              <li class="google-plus"><a href="#"><i class="icon-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="probootstrap-teacher text-center probootstrap-animate">
+
+          <div class="text">
+            <h3>Mrs Anuoluwapo Olowookere</h3>
+            <p>Class Teacher - Year 1</p>
+            <ul class="probootstrap-footer-social">
+              <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
+              <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li class="instagram"><a href="#"><i class="icon-instagram2"></i></a></li>
+              <li class="google-plus"><a href="#"><i class="icon-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="probootstrap-teacher text-center probootstrap-animate">
+
+          <div class="text">
+            <h3>Mr Oladimeji Olanihun</h3>
+            <p>Class Teacher - Year 6</p>
+            <ul class="probootstrap-footer-social">
+              <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
+              <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li class="instagram"><a href="#"><i class="icon-instagram2"></i></a></li>
+              <li class="google-plus"><a href="#"><i class="icon-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="probootstrap-teacher text-center probootstrap-animate">
+
+          <div class="text">
+            <h3>Mr Sunday Agbalaya</h3>
+            <p>Creative Arts Instructor</p>
+            <ul class="probootstrap-footer-social">
+              <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
+              <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li class="instagram"><a href="#"><i class="icon-instagram2"></i></a></li>
+              <li class="google-plus"><a href="#"><i class="icon-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="probootstrap-teacher text-center probootstrap-animate">
+
+          <div class="text">
+            <h3>Mr Yaovi Aguidji</h3>
+            <p>French Language Instructor</p>
+            <ul class="probootstrap-footer-social">
+              <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
+              <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li class="instagram"><a href="#"><i class="icon-instagram2"></i></a></li>
+              <li class="google-plus"><a href="#"><i class="icon-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="row">
 
       <div class="clearfix visible-sm-block visible-xs-block"></div>
@@ -126,8 +258,8 @@ include 'header.php';
         <div class="probootstrap-teacher text-center probootstrap-animate">
 
           <div class="text">
-            <h3>Mr John Ekanem</h3>
-            <p>Creative Arts Instructor</p>
+            <h3>Dr Asa Deji - Haltism</h3>
+            <p>Special Needs Consultant</p>
             <ul class="probootstrap-footer-social">
               <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
               <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
@@ -141,8 +273,8 @@ include 'header.php';
         <div class="probootstrap-teacher text-center probootstrap-animate">
 
           <div class="text">
-            <h3>Mr Segun Adetunji </h3>
-            <p>Music Instructor</p>
+            <h3>Speech Quest  </h3>
+            <p>Phonics Consultant </p>
             <ul class="probootstrap-footer-social">
               <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
               <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
